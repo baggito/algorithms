47b80f5123ea9cc13c5db37c107210b09ae87ca1
Bagrat <sakanyan.bagrat@gmail.com>
2020-06-07 11:18:20 -0700

    sort binary array in linear time

---
+++ sortBinaryArrayInLinearTime/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// given binary array
+	arr := []int{1, 0, 1, 0, 1, 0, 0, 1}
+
+	// sort
+	sort(arr)
+
+	fmt.Println("The end of algorithm")
+}
+
+// sort array with O(n) complexity
+func sort(arr []int) {
+	start := 0
+	end := len(arr) - 1
+
+	for start < end {
+
+		if arr[start] == 1 && arr[end] == 0 {
+			swap(&arr, start, end)
+			start++
+			end--
+			continue
+		} else if arr[start] == 1 {
+			end--
+		} else if arr[start] == 0 {
+			start++
+		} else {
+			start++
+			end--
+		}
+	}
+
+	fmt.Println(arr)
+}
+
+// utility function to swap two elements of given array
+func swap(arr *[]int, i, j int) {
+	tmp := (*arr)[i]
+	(*arr)[i] = (*arr)[j]
+	(*arr)[j] = tmp
+}
